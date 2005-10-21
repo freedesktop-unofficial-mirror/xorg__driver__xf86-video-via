9f873c4cdce8c036c24503d6310c54425ce33435
Adam Jackson <ajax@nwnk.net>
2005-10-21 18:40:19 +0000

    Bug #4829: Remove some useless DRI symbol checks.

--- src/via_dri.c
+++ src/via_dri.c
@@ -601,7 +601,6 @@ Bool VIADRIScreenInit(ScreenPtr pScreen)
     /* Check that the GLX, DRI, and DRM modules have been loaded by testing
     * for canonical symbols in each module. */
     if (!xf86LoaderCheckSymbol("GlxSetVisualConfigs")) return FALSE;
-    if (!xf86LoaderCheckSymbol("DRIScreenInit"))       return FALSE;
     if (!xf86LoaderCheckSymbol("drmAvailable"))        return FALSE;
     if (!xf86LoaderCheckSymbol("DRIQueryVersion")) {
 	xf86DrvMsg(pScreen->myNum, X_ERROR,
