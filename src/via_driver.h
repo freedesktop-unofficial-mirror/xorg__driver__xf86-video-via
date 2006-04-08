e58d7f5fc1e97f326e61c8c899446fea8b19ba9f
Adam Jackson <ajax@nwnk.net>
2006-04-08 01:12:23 +0000

    Bump to 0.2.1 for Xv changes.

--- src/via_driver.h
+++ src/via_driver.h
@@ -68,7 +68,7 @@
 #define DRIVER_NAME     "via"
 #define VERSION_MAJOR   0
 #define VERSION_MINOR   2
-#define PATCHLEVEL      0
+#define PATCHLEVEL      1
 #define VIA_VERSION     ((VERSION_MAJOR<<24) | (VERSION_MINOR<<16) | PATCHLEVEL)
 
 #ifdef VIA_HAVE_EXA
